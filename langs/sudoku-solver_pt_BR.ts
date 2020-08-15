30f6c19ff3f03894246819ab8742839a31e05990
Fabio A. De Muzio Tobich <fabiomt@gmail.com>
2020-08-15 18:24:27 -0300

    adding Brazilian Portuguese translation

---
+++ langs/sudoku-solver_pt_BR.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>FormAbout</name>
+    <message>
+        <location filename="../FormAbout.ui" line="31"/>
+        <source>Sudoku solver</source>
+        <translation>Sudoku Solver</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="131"/>
+        <source>License: GPLv3+</source>
+        <translation>Licença: GPLv3+</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="208"/>
+        <source>Visit web site</source>
+        <translation>Visitar página web</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="226"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="15"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="18"/>
+        <source>Version:</source>
+        <translation>Versão:</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="19"/>
+        <source>Date build:</source>
+        <translation>Data de construção:</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="20"/>
+        <source>All rights reserved.</source>
+        <translation>Todos os direitos reservados.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="30"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="46"/>
+        <location filename="../mainwindow.cpp" line="337"/>
+        <source>Find a solution</source>
+        <translation>Solucionar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="167"/>
+        <source>Clear the Board</source>
+        <translation>Limpar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="151"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="23"/>
+        <source>Sudoku solver v%1</source>
+        <translation>Sudoku Solver v%1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Stop searching</source>
+        <translation>Parar busca</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="322"/>
+        <location filename="../mainwindow.cpp" line="331"/>
+        <source>Sudoku solver</source>
+        <translation>Sudoku Solver</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="322"/>
+        <source>Solving Sudoku is completed!</source>
+        <translation>Solução do Sudoku completada!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="331"/>
+        <source>The solution to this Sudoku is not found!</source>
+        <translation>A solução para este Sudoku não foi encontrada!</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../main.cpp" line="17"/>
+        <location filename="../main.cpp" line="24"/>
+        <source>Sudoku solver</source>
+        <translation>Sudoku Solver</translation>
+    </message>
+</context>
+</TS>
